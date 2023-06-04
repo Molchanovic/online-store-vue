52f635c54ede025c2d2639ac809ae5a60c7fe342
Molchanovic <sir.puptik2018@yandex.ru>
2023-06-04 19:06:39 +0700

    create layout sideBar pages

---
+++ src/layout/main-layout.vue
@@ -0,0 +1,17 @@
+<template>
+  <UiHeader/>
+  <main class="main-block">
+    <router-view/>
+  </main>
+</template>
+<script>
+ import UiHeader from '../components/UiHeader.vue'
+ import SideBar from '../components/SideBar.vue'
+
+export default ({
+ components: {
+  UiHeader,
+  SideBar
+ }
+})
+</script>
