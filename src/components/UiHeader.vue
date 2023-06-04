b73a1015a76caff244885daa5c637ffab19515a6
Molchanovic <sir.puptik2018@yandex.ru>
2023-06-04 19:06:52 +0700

    added edits

---
+++ src/components/UiHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="header">
+    <div class="header-wrapper">
+      <h1 class="header-title">Ui Kit</h1>
+    </div>
+  </header>
+</template>
+<script>
+export default defineComponent({
+  props: {
+    title: {
+      type: String,
+    }
+  },
+ data() {
+  return {
+
+  }
+ }
+})
+</script>
