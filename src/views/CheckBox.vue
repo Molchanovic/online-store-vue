4b465be4882c86ea6e646783c2a2a8d7d2acbe54
Molchanovic <sir.puptik2018@yandex.ru>
2023-06-17 17:45:30 +0700

    added change

--- src/views/CheckBox.vue
+++ src/views/CheckBox.vue
@@ -2,5 +2,39 @@
   <div class="wrapper-main">
     <h1>CheckBox</h1>
     <hr />
+    <UiCheckBox
+      label="Путин"
+      value="Putin"
+      :checked="isChecked"
+      id="12"
+      name="president"
+      @change="isChecked = !isChecked"
+    />
+    <UiCheckBox
+      label="Мама"
+      value="Mama"
+      :checked="isCheckedTwo"
+      id="10"
+      name="mama"
+      @change="isCheckedTwo = !isCheckedTwo"
+    />
   </div>
 </template>
+<script>
+import UiCheckBox from "../components/ui/CheckBox/UiCheckBox.vue";
+import { ref } from "vue";
+export default {
+  components: {
+    UiCheckBox,
+  },
+  setup() {
+    const isChecked = ref(false);
+    const isCheckedTwo = ref(false);
+
+    return {
+      isChecked,
+      isCheckedTwo,
+    };
+  },
+};
+</script>
